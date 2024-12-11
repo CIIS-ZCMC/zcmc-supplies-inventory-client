36eb29156b7934a424a2d4de3ea18e89daab0ec1
TjayAmit <tristanjayamit0813@gmail.com>
2024-12-11 15:20:14 +0800

    setup for development testing

--- src/Services/Config.jsx
+++ src/Services/Config.jsx
@@ -80,6 +80,6 @@ export const BASE_URL = {
   development_landing_page: "http://192.168.5.1:5170", // This will be the landing page
   production_landing_page: "https://zcmc.online", // This will be the production landing page url
   production: "https://api_name.zcmc.online/api/", // Change the sub domain name to your prefer name
-  development: "http://192.168.5.1:5550/api", // You can change the port or ip here
+  development: "http://192.168.5.1:5166/api", // You can change the port or ip here
   local: "http://localhost:8000/api", // You can change the port or ip here
 };
