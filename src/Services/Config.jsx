b6d5c451f87e573d28e634b5d9955cacc922f91c
TjayAmit <tristanjayamit0813@gmail.com>
2024-12-17 15:43:24 +0800

    update production

--- src/Services/Config.jsx
+++ src/Services/Config.jsx
@@ -81,9 +81,9 @@ export const API = {
  * use the production for your system url
  */
 export const BASE_URL = {
-  development_landing_page: "http://192.168.5.1:5170", // This will be the landing page
-  production_landing_page: "https://zcmc.online", // This will be the production landing page url
-  production: "https://api_name.zcmc.online/api/", // Change the sub domain name to your prefer name
+  development_landing_page: "http://192.168.5.1:5170/api", // This will be the landing page
+  production_landing_page: "https://zcmc.online/api", // This will be the production landing page url
+  production: "https://api_name.zcmc.online/api", // Change the sub domain name to your prefer name
   development: "http://192.168.5.1:5166/api", // You can change the port or ip here
   local: "http://localhost:8000/api", // You can change the port or ip here
 };
